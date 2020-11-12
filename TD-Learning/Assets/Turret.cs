1885b3a550215710a41b327ff2276323daf64c6d
chewgoji <willi0955@gmail.com>
2020-11-12 18:43:31 +0800

    E05 SHOOTING complete
    GameObject effectIns = (GameObject)
    
    object casting?

--- TD-Learning/Assets/Turret.cs
+++ TD-Learning/Assets/Turret.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+
+    [Header("Attributes")]
+
     public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
+
     public Transform partToRotate;
     public float turnSpeed = 10f;
 
     public string enemytag = "Enemy";
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -53,6 +66,23 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+
+        if (bullet != null)
+            bullet.Seek(target);
     }
 
     void OnDrawGizmosSelected()
